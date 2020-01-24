c7da1481e45f9661fe165222f6d156e842536397
Stippler <christian.stippel@gmail.com>
2020-01-24 20:50:57 +0100

    add plots for validation

--- cnn.py
+++ cnn.py
@@ -98,3 +98,27 @@ plt.ylabel("Training loss", fontsize=15)
 plt.xticks(fontsize=15)
 plt.yticks(fontsize=15)
 plt.show()
+
+# %% plot validation
+
+plt.figure(figsize=(7, 4))
+plt.plot([i + 1 for i in range(n_epochs)], history.history['val_acc'], '-o', c='k', lw=2, markersize=9)
+plt.grid(True)
+plt.title("Validation accuracy with epochs\n", fontsize=18)
+plt.xlabel("Training epochs", fontsize=15)
+plt.ylabel("Training accuracy", fontsize=15)
+plt.xticks(fontsize=15)
+plt.yticks(fontsize=15)
+plt.show()
+
+# %% plot loss
+
+plt.figure(figsize=(7, 4))
+plt.plot([i + 1 for i in range(n_epochs)], history.history['val_loss'], '-o', c='k', lw=2, markersize=9)
+plt.grid(True)
+plt.title("Validation loss with epochs\n", fontsize=18)
+plt.xlabel("Training epochs", fontsize=15)
+plt.ylabel("Validation loss", fontsize=15)
+plt.xticks(fontsize=15)
+plt.yticks(fontsize=15)
+plt.show()
