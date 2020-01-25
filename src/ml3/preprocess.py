5c9422cde8220db39d193eec04ef0b102aaaf980
rafael <rafael@rafael>
2020-01-25 21:14:51 +0100

    automating folder generation

--- src/ml3/preprocess.py
+++ src/ml3/preprocess.py
@@ -1,5 +1,5 @@
 import argparse
-
+import os
 import split_folders
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
@@ -7,14 +7,14 @@ import ml3.config as cfg
 
 
 def split_data(folder: str):
-    split_folders.ratio('data/original/' + folder, 'data/split/' + folder, seed=1337,
+    split_folders.ratio('data/original/' + folder, 'data/splits/' + folder, seed=1337,
                         ratio=(cfg.TRAIN, cfg.VALIDATION))
 
 
 def train_data_generator(folder: str):
     train_data_generator = ImageDataGenerator(rescale=cfg.SCALE)
     generator = train_data_generator.flow_from_directory(
-        'data/split/' + folder + '/train',
+        'data/splits/' + folder + '/train',
         target_size=cfg.IMAGE_SHAPE,
         batch_size=cfg.BATCH_SIZE,
         class_mode='categorical')
@@ -24,7 +24,7 @@ def train_data_generator(folder: str):
 def validation_data_generator(folder: str):
     validation_data_generator = ImageDataGenerator(rescale=cfg.SCALE)
     generator = validation_data_generator.flow_from_directory(
-        'data/split/' + folder + '/val',
+        'data/splits/' + folder + '/val',
         target_size=cfg.IMAGE_SHAPE,
         class_mode='categorical')
     return generator
@@ -34,5 +34,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--dir', type=str, required=True, help='name of folder')
     args = parser.parse_args()
+
+    try:
+        # Create target directory
+        os.mkdir('data/splits')
+        print("Created directory")
+    except FileExistsError:
+        print("Directory already exists")
 
     split_data(args.dir)
